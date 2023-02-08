81e9fe878a258e4382f148ad6bce6e51ca46e794
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-02-08 05:26:15 -0600

    commit tree 01a6fa646a9f493bf6e8950dcf8645b8f6511f79

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_resp
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_resp
+
+    
+
+    private
+
+    def render_not_found_resp(exception)
+        render json: { errors: "#{exception.model} not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_resp(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+
 end
